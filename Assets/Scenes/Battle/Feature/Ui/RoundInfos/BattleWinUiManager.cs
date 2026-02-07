2d5853cee4ae2866cee4534517e24b8b37b91476
JioBani <spdlqj8876@gmail.com>
2026-02-07 23:16:13 +0900

    TACD-146 임무 완료 UI 애니메이션 추가

--- Assets/Scenes/Battle/Feature/Ui/RoundInfos/BattleWinUiManager.cs
+++ Assets/Scenes/Battle/Feature/Ui/RoundInfos/BattleWinUiManager.cs
@@ -1,12 +1,17 @@
 using Common.Scripts.GlobalEventBus;
+using Common.Scripts.SceneDataManager;
+using DG.Tweening;
 using Scenes.Battle.Feature.Events.RoundEvents;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Scenes.Battle.Feature.Ui.RoundInfos
 {
     public class BattleWinUiManager : MonoBehaviour
     {
         [SerializeField] GameObject battleWinPanel;
+
+        private const float ShowAnimationDuration = 0.5f;
 
         private void OnEnable()
         {
@@ -19,9 +24,31 @@ namespace Scenes.Battle.Feature.Ui.RoundInfos
             GlobalEventBus.Unsubscribe<OnBattleWinEventDto>(OnBattleWin);
         }
 
+        private void OnDestroy()
+        {
+            battleWinPanel.transform.DOKill();
+        }
+
         private void OnBattleWin(OnBattleWinEventDto _)
         {
+            ShowPanel();
+        }
+
+        private void ShowPanel()
+        {
+            battleWinPanel.transform.DOComplete();
+            battleWinPanel.transform.localScale = Vector3.one * 0.5f;
             battleWinPanel.SetActive(true);
+            battleWinPanel.transform.DOScale(Vector3.one, ShowAnimationDuration).SetEase(Ease.OutBack);
+        }
+
+        /// <summary>
+        /// 확인 버튼 클릭 시 BattleFront 씬으로 이동
+        /// </summary>
+        public void OnConfirmButtonClick()
+        {
+            SceneDataManager.Instance.ClearAll();
+            SceneManager.LoadScene("BattleFront");
         }
     }
 }
--- Assets/Scenes/Battle/Feature/Ui/RoundInfos/GameOverUiManager.cs
+++ Assets/Scenes/Battle/Feature/Ui/RoundInfos/GameOverUiManager.cs
@@ -1,12 +1,17 @@
 using Common.Scripts.GlobalEventBus;
+using Common.Scripts.SceneDataManager;
+using DG.Tweening;
 using Scenes.Battle.Feature.Events.RoundEvents;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace Scenes.Battle.Feature.Ui.RoundInfos
 {
     public class GameOverUiManager : MonoBehaviour
     {
         [SerializeField] GameObject gameOverPanel;
+
+        private const float ShowAnimationDuration = 0.5f;
 
         private void OnEnable()
         {
@@ -19,9 +24,31 @@ namespace Scenes.Battle.Feature.Ui.RoundInfos
             GlobalEventBus.Unsubscribe<OnBattleLoseEventDto>(OnBattleLose);
         }
 
+        private void OnDestroy()
+        {
+            gameOverPanel.transform.DOKill();
+        }
+
         private void OnBattleLose(OnBattleLoseEventDto _)
         {
+            ShowPanel();
+        }
+
+        private void ShowPanel()
+        {
+            gameOverPanel.transform.DOComplete();
+            gameOverPanel.transform.localScale = Vector3.one * 0.5f;
             gameOverPanel.SetActive(true);
+            gameOverPanel.transform.DOScale(Vector3.one, ShowAnimationDuration).SetEase(Ease.OutBack);
+        }
+
+        /// <summary>
+        /// 확인 버튼 클릭 시 BattleFront 씬으로 이동
+        /// </summary>
+        public void OnConfirmButtonClick()
+        {
+            SceneDataManager.Instance.ClearAll();
+            SceneManager.LoadScene("BattleFront");
         }
     }
 }   
