d03757a3b599f88a9ce0dd2113d597a7da310b44
JioBani <spdlqj8876@gmail.com>
2026-02-24 00:15:11 +0900

    TACD-210 feat: HP 바 성급별 테두리 색 변경 기능 추가
    ## 전체 목표
    소환수 성급 표시 기능 구현
    
    ## 작업 필요 이유
    소환수의 성급을 시각적으로 구분할 수 있도록 HP 바 테두리 색상을 성급별로 다르게 표시해야 한다
    
    ## 작업 세부 내용
    - Assets/Scenes/Battle/Feature/Unit/HealthBar/Scripts/HealthBar.cs (수정): SetStarGrade() 메서드 추가. 성급별 테두리 색상을 SerializeField로 노출하여 에디터에서 조정 가능하게 함 (1성: 회색, 2성: 초록색, 3성: 금색)

--- Assets/Scenes/Battle/Feature/Unit/HealthBar/Scripts/HealthBar.cs
+++ Assets/Scenes/Battle/Feature/Unit/HealthBar/Scripts/HealthBar.cs
@@ -7,15 +7,34 @@ namespace Scenes.Battle.Feature.Units.HealthBars
         [SerializeField] private SpriteRenderer outlineSprite;
         [SerializeField] private SpriteRenderer insideSprite;
 
+        [Header("성급별 테두리 색")]
+        [SerializeField] private Color star1Color = Color.gray;
+        [SerializeField] private Color star2Color = Color.green;
+        [SerializeField] private Color star3Color = new Color(1f, 0.84f, 0f);
+
         public void Display(float rate)
         {
             if (rate <= 0)
             {
                 rate = 0;
             }
-            
+
             float sizeX = outlineSprite.size.x * rate;
             insideSprite.size = new Vector2(sizeX, insideSprite.size.y);
+        }
+
+        /// <summary>
+        /// 성급에 따라 HP 바 테두리 색을 변경한다.
+        /// 1성: 회색, 2성: 금색, 3성 이상: 주황색.
+        /// </summary>
+        public void SetStarGrade(int star)
+        {
+            outlineSprite.color = star switch
+            {
+                <= 1 => star1Color,
+                2    => star2Color,
+                _    => star3Color,
+            };
         }
     }
 }
