3326cac7763e38c91088123da9d5822f495cb1ec
JioBani <spdlqj8876@gmail.com>
2026-03-28 00:03:53 +0900

    TACD-290 feat: UnitAnimator 컴포넌트 제작
    ## 전체 목표
    유닛 애니메이션 프레임워크 제작 (TACD-287)
    
    ## 작업 필요 이유
    ActionStateController의 상태 변경에 연동하여 애니메이션을 자동 전환하는 런타임 컴포넌트가 필요하다.
    IStateListener 패턴을 따라 기존 구독 구조와 일관성을 유지한다.
    
    ## 작업 세부 내용
    - Assets/Scenes/Battle/Feature/Unit/Animation/UnitAnimator.cs (신규): IStateListener<ActionStateType> 구현. Init()으로 성급별 OverrideController 할당, OnStateEnter에서 animator.Play(state.ToString())으로 클립 자동 전환
    - Claude/work/TACD-290/작업정의.md (신규): TACD-290 작업정의

---
+++ Assets/Scenes/Battle/Feature/Unit/Animation/UnitAnimator.cs
@@ -0,0 +1,43 @@
+using Common.Data.Units.UnitDefinitions;
+using Common.Scripts.StateBase;
+using Scenes.Battle.Feature.Units.ActionStates;
+using UnityEngine;
+
+namespace Scenes.Battle.Feature.Units
+{
+    /// <summary>ActionState 변경 시 애니메이션을 자동 전환하는 컴포넌트.</summary>
+    public class UnitAnimator : MonoBehaviour, IStateListener<ActionStateType>
+    {
+        [SerializeField] private Animator animator;
+        [SerializeField] private ActionStateController actionStateController;
+
+        private void Awake()
+        {
+            actionStateController.RegisterListener(this);
+        }
+
+        /// <summary>유닛 소환 시 호출된다. 데이터에서 해당 성급의 OverrideController를 Animator에 할당한다.</summary>
+        public void Init(UnitDefinitionData data, int star)
+        {
+            animator.runtimeAnimatorController = data.GetAnimatorByStar(star);
+        }
+
+        void IStateListener<ActionStateType>.OnStateEnter(ActionStateType stateType)
+        {
+            animator.Play(stateType.ToString());
+        }
+
+        void IStateListener<ActionStateType>.OnStateRun(ActionStateType stateType)
+        {
+        }
+
+        void IStateListener<ActionStateType>.OnStateExit(ActionStateType stateType)
+        {
+        }
+
+        private void OnDestroy()
+        {
+            actionStateController.UnregisterListener(this);
+        }
+    }
+}
